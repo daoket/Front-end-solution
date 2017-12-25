a732aecdb9d5f90805018503beb8da6c4dac7dfc
daoket <daoket@163.com>
2017-12-25 15:30:16 +0800

    add audio

---
+++ lx活动常用组件/登录绑定/js/config_deploy.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2017/8/28 0023.
+ */
+var CFG = (function() {
+     var cfg = {
+        BAIDU: '773c2fb525481d4d0c8e981c6168c11e',
+        
+        getGameTimes: '//61.191.24.229:5021/autumn/activity/getlotterynum', // 查询是否绑定手机号
+        
+    };
+    return cfg;
+})();
