72bf1f13bfca37e2faa50cb854f6d9b1bd5d863d
daoket <daoket@163.com>
2018-03-05 11:43:24 +0800

    :zap:

--- python/读取和写入Excel/read.py
+++ python/读取和写入Excel/read.py
@@ -1,5 +1,17 @@
 from openpyxl import load_workbook
-wb = load_workbook('test.xlsx')
-sheet = wb.get_sheet_by_name('Sheet')
-for i in sheet['C']:
-    print(i.value, end=' ')
+
+
+"""
+@desc 读取excel文件
+@args excelName 读取文件名
+@args sheetName 读取sheet名
+@args readLine 数字为列 字母为行
+"""
+def readExcel(excelName, sheetName, readLine):
+    wb = load_workbook(excelName)
+    sheet = wb.get_sheet_by_name(sheetName)
+    for i in sheet[readLine]:
+        print(i.value, end=',')
+
+
+readExcel('a.xlsx', 'Sheet2', 'c')
--- python/读取和写入Excel/write.py
+++ python/读取和写入Excel/write.py
@@ -1,8 +1,19 @@
 from openpyxl import Workbook
-wb = Workbook()
-sheet = wb.active
-sheet['C3'] = 'hello world'
-for i in range(10):
-    sheet['A%d' % (i+1)].value = i + 1
-sheet['E1'].value = '=SUM(A:A)'
-wb.save('test.xlsx')
+
+"""
+@desc 写入excel文件
+@args excelName 读取文件名
+"""
+
+
+def writeExcel(excelName):
+    wb = Workbook()
+    sheet = wb.active
+    sheet['C3'] = 'hello world'
+    for i in range(10):
+        sheet['A%d' % (i + 1)].value = i + 1
+    sheet['E1'].value = '=SUM(A:A)'
+    wb.save(excelName)
+
+
+writeExcel('b.xlsx')
